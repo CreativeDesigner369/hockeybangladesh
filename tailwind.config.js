8a69da1ca2cfef9522ce5e875d484bce6a5362ea
CreativeDesigner369 <creative.designer281@gmail.com>
2024-01-29 20:51:34 +0600

    All files added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
+  },
+};
